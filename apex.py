b77f8b64510a54859aad7a42a391876d60868854
lingyun67 <64645371+lingyun67@users.noreply.github.com>
2023-12-30 11:25:10 +0800

    Add files via upload

---
+++ apex.py
@@ -0,0 +1,87 @@
+import random
+import time
+
+# 定义艺术字标题
+title = """
+\033[1;36mApex Legends角色抽取选择困难症解决方法——随机生成角色选择之chatgpt享受者\033[0m
+     _                      _                              _     
+    / \\   _ __   _____  __ | |    ___  __ _  ___ _ __   __| |___ 
+   / _ \\ | '_ \\ / _ \\ \\/ / | |   / _ \\/ _` |/ _ \\ '_ \\ / _` / __|
+  / ___ \\| |_) |  __/>  <  | |__|  __/ (_| |  __/ | | | (_| \\__ \\
+ /_/   \\_\\ .__/ \\___/_/\\_\\ |_____\\___|\\__, |\\___|_| |_|\\__,_|___/
+         |_|                          |___/                      
+"""
+print(title)
+
+
+# 所有游戏角色及其分类
+characters = {
+    '进攻': ['进攻-班加罗尔', '进攻-暴雷', '进攻-艾许', '进攻-疯玛吉', '进攻-弹道'],
+    '游击': ['游击-恶灵', '游击-动力小子', '游击-地平线', '游击-探路者', '游击-瓦尔基里', '游击-亡灵'],
+    '侦察': ['侦察-寻血猎犬', '侦察-密客', '侦察-希尔', '侦察-万蒂奇'],
+    '支援': ['支援-直布罗陀', '支援-命脉', '支援-纽卡斯尔', '支援-罗芭', '支援-幻象', '支援-导线管'],
+    '控制': ['控制-侵蚀', '控制-沃特森', '控制-兰伯特', '控制-卡特莉丝']
+}
+
+# 抽取规则1：从某一类别中随机抽取3个角色
+def rule1():
+    category = random.choice(list(characters.keys()))
+    print(f"\033[1;33m已选用抽取规则1，正在抽取角色类别。\033[0m")
+    time.sleep(1)
+    print(f"\033[1;33m抽取到的角色类别：{category}\033[0m")
+    time.sleep(1)
+    input("\033[1;37m按下回车以继续抽取角色\033[0m")
+    print("\033[1;33m正在随机抽取角色；\033[0m")
+    time.sleep(1)
+    selected_characters = random.sample(characters[category], 3)
+    random.shuffle(selected_characters)
+    return category, selected_characters
+
+# 抽取规则2：直接随机抽取3个角色
+def rule2():
+    print("\033[1;33m已选用抽取规则2，直接随机抽取三个角色；\033[0m")
+    time.sleep(1)
+    selected_characters = random.sample([char for chars in characters.values() for char in chars], 3)
+    random.shuffle(selected_characters)
+    return selected_characters
+
+# 初始化抽取次数计数器
+draw_count = 1
+
+while True:
+    # 提供规则解释
+    if draw_count == 1:
+        print("\033[1;37m抽取规则1：从某一类别中随机抽取3个角色。\033[0m")
+        print("\033[1;37m抽取规则2：直接随机抽取3个角色。\033[0m")
+
+    if draw_count != 1:
+        print(f"\033[1;37m当前是第{draw_count}次抽取\033[0m")
+    # 更新抽取次数计数器
+    draw_count += 1
+
+    # 询问用户使用哪种规则
+    rule_type = input("\033[1;37m请选择抽取规则 (1 或 2)，或输入 'exit' 退出程序：\033[0m")
+
+    if rule_type.lower() == 'exit':
+        break  # 用户输入 'exit' 时退出循环
+
+    # 根据用户选择执行相应规则
+    if rule_type == '1':
+        category, selected_characters = rule1()
+        print("\033[1;37m抽取结果：\033[0m")
+        for i, player in enumerate(['玩家1', '玩家2', '玩家3']):
+            time.sleep(1)
+            print(f"\033[1;34m{player}：{selected_characters[i]}\033[0m")
+    elif rule_type == '2':
+        selected_characters = rule2()
+        print("\033[1;37m抽取结果：\033[0m")
+        for i, player in enumerate(['玩家1', '玩家2', '玩家3']):
+            time.sleep(1)
+            print(f"\033[1;34m{player}：{selected_characters[i]}\033[0m")
+    else:
+        print("\033[1;31m无效的选择，请输入 '1' 或 '2'.\033[0m")
+
+    time.sleep(1)
+    input("\033[1;37m按下回车以继续，或输入 'exit' 退出程序：\033[0m")
+
+print("\033[1;32m程序结束。\033[0m")
